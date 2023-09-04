0ae5fd65cbd53670c6845292056ea64153f42b19
Ashish Bhusal <ashishb@arista.com>
2023-09-04 18:54:32 +0530

    Go: Add exercises of slices and variadic functions in Go

---
+++ go/card-tricks/card_tricks.go
@@ -0,0 +1,48 @@
+package cards
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int {
+    return []int{2,6,9}
+}
+
+// indexInBound checks if the given index is valid index in the slice
+func indexInBound(slice []int, index int) bool {
+    if index < 0 || index >= len(slice) {
+        return false;
+    }
+    return true;
+}
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+    if !indexInBound(slice, index) {
+        return -1
+    }
+    return slice[index]
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+    if !indexInBound(slice, index) {
+        slice = append(slice, value)
+    } else {
+        slice[index] = value
+    }
+    return slice
+}
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(slice []int, values ...int) []int {
+    slice = append(values, slice...)
+    return slice
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+    if !indexInBound(slice, index) {
+        return slice
+    }
+    return append(slice[:index], slice[index+1:]...)
+}
